2d4860eecc4b07d088aa3eac4c94f8cccb2171c7
VanshWebDev <vanshvanshkumar39@gmail.com>
2024-03-06 07:37:11 +0530

    added some validation

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let logStyler=()=>{
-    
-}
-
-
-let style = {
-  color: "red",
-  "font-size": "40px",
-  background: "orange",
-  "border-radius": "10px",
-  "border-right": "green",
-  color: "white",
-  padding: "10px",
+let logStyler = (styleObj, text) => {
+  try {
+    //validation
+    if (typeof styleObj !== "object") {
+      throw new Error("Style object is required");
+    } else if (!Object.keys(styleObj).length) {
+      throw new Error("Style object should not be empty");
+    } else if (!text) {
+      throw new Error("second parameter text is required");
+    }
+    //run if validation passed
+    let style = styleObj;
+    let styleString = "";
+    for (let prop in style) {
+      styleString += `${prop}: ${style[prop]};`;
+    }
+    console.log(`%c${text}`, styleString);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-let styleString = "";
-for (let prop in style) {
-  styleString += `${prop}: ${style[prop]};`;
-}
-
-console.log(`%cName`, styleString);
+export { logStyler };
---
+++ test.js
@@ -0,0 +1,9 @@
+import { logStyler } from "./index.js";
+
+logStyler(
+  {
+    color: "red",
+    background: "yellow",
+  },
+);
+
